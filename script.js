95ed957a488a812e3a57035a97eebf10b3a67de1
Cristofer Philippi <cphilippi.dev@gmail.com>
2026-07-03 00:07:44 -0300

    aulas iniciais de JS

---
+++ script.js
@@ -0,0 +1,12 @@
+//alert("tudo bem Cristofer?");
+var hora = new Date().getHours();
+
+if (hora < 12) {
+  saudacao = "Bom dia";
+} else if (hora < 18) {
+  saudacao = "Boa tarde";
+} else {
+  saudacao = "Boa noite";
+}
+
+document.getElementById("mensagem").innerHTML = saudacao;
