b88b4e6aea0c9a1cd5901ff7535e3cf4b9f40781
Cristofer Philippi <cphilippi.dev@gmail.com>
2026-07-06 14:39:54 -0300

    aula inicial de arrays

---
+++ Arrays/arrays.js
@@ -0,0 +1,11 @@
+let nomes = ["Cristofer", "Luana", "Celina"];
+
+// adiciona novos itens:
+nomes.push("novo item");
+console.log(nomes[3]);
+// insere dado na posição:
+nomes[0] = "Teste";
+console.log(nomes[0]);
+//deletar item do array:
+delete nomes[0];
+console.log(nomes[0]);
