6bde18f1c7ff3c585ecd88994a9e1dc12658f23e
Cristofer Philippi <cphilippi.dev@gmail.com>
2026-07-07 20:54:33 -0300

    concatenação e template string

---
+++ JS_variados/script.js
@@ -0,0 +1,13 @@
+//concatenação
+
+const nome = "Cristofer";
+const numeroCarrinho = 2;
+//console.log(nome + ' - ' + numeroCarrinho)
+
+//template string - reconhece até a quebra de linha
+
+const texto = `Bem vindo ${nome} 
+você tem: 
+${numeroCarrinho} produtos no Carrinho`;
+
+console.log(texto);
