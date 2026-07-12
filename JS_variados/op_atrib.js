68e500fd6bcd6ad97c712a5650bf75c53f25b280
Cristofer Philippi <cphilippi.dev@gmail.com>
2026-07-12 15:10:55 -0300

    class operador de atribuição

---
+++ JS_variados/op_atrib.js
@@ -0,0 +1,23 @@
+// operador de atribuição
+
+let salario = 500;
+let bonus = 200;
+
+//salario = salario + bonus
+// ou
+//salario += bonus
+
+let desconto = 100;
+//salario = salario - desconto
+// ou
+//salario -= desconto
+
+let mult = 2;
+//salario = salario * mult
+// ou
+//salario *= mult
+
+let div = 2;
+//salario = salario / div
+//ou
+//salario /= div
